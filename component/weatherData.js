7d06023d0ace325be5734194c972a64d656c4de4
rajchatterz <iamraj.space@gmail.com>
2023-12-04 11:08:27 +0530

    start with the weather app

---
+++ component/weatherData.js
@@ -0,0 +1,12 @@
+import { View } from "react-native"
+
+const API_KEY = '97abbfee3233b110cffbb6407cc22df3'
+const BASE_URL='https://api.openweathermap.org/data/2.5/weather'
+function weatherData() { 
+    const []
+    return (
+        <View></View>
+    )
+}
+
+export default weatherData
