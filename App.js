1d842e28a65a5babcfede4b9a054da1d364f26be
rajchatterz <iamraj.space@gmail.com>
2023-12-04 18:50:52 +0530

    done with the app

--- App.js
+++ App.js
@@ -1,14 +1,18 @@
 import { View, Text, TextInput, Button,ActivityIndicator,StyleSheet, ImageBackground, Platform, TouchableOpacity } from 'react-native'
 import React, { useState } from 'react'
-import { StatusBar } from 'react-native'
-import {Ionicons } from '@expo/vector-icons'
+import { useFonts} from 'expo-font'
 import WeatherSearch from './component/WeatherSearch'
 const WeatherApp = () => {
   const [city, setCity] = useState('')
   const [weatherData, setWeatherData] = useState(null)
   const [loading, setLoading] = useState(false)
   const [error, setError] = useState(false)
-  
+  const [fontLoaded] = useFonts({
+    'Outfit-Bold':require('./assets/font/Outfit-Bold.ttf')
+  })
+  if (!fontLoaded) {
+    return null
+  }
   const fetchWeather = async () => {
     setLoading(true)
     setError(false) 
@@ -42,8 +46,13 @@ const WeatherApp = () => {
     }
   }
 
-  let date1 = new Date((weatherData.dt * 1000))
-  let date = date1.toLocaleString()
+
+  let date = null
+  if (weatherData && weatherData.dt) {
+    const date1 = new Date(weatherData.dt * 1000)
+    date = date1.toLocaleDateString() + ' ' + date1.toLocaleTimeString()
+  }
+  console.log(weatherData)
   return (
       <ImageBackground style={styles.imageContainer} source={require('./assets/image/bcg.jpeg')}>
         <View style={styles.viewContainer}>
@@ -51,18 +60,27 @@ const WeatherApp = () => {
           <View style={styles.viewContainer}>
             <View style={styles.tempContainer}>
               <View style={styles.mainContainer}>
-                <Text style={{fontSize:40, fontWeight:'bold',color:'white',fontFamily:''}}>{loading ? (<ActivityIndicator />) : error ? (<Text>Error found</Text>) : weatherData ? <Text>{ weatherData.name}</Text>:null}</Text>
-                <Text>{loading ? (<ActivityIndicator />) : error ? (<Text>Error found</Text>) : weatherData ? <Text>{date}</Text>:null}</Text>
+                <Text style={{fontSize:50, fontWeight:'bold',color:'#f6e2ce',fontFamily:'Outfit-bold'}}>{loading ? (<ActivityIndicator />) : error ? (<Text>No Location Found</Text>) : weatherData ? <Text>{ weatherData.name}</Text>:null}</Text>
+                <Text style={{color:'#f8f8f8',fontSize:20,fontWeight:'500'}}>{loading ? (<ActivityIndicator />) : error ? (<Text>Invalid Date</Text>) : weatherData ? <Text>{date}</Text>:null}</Text>
               </View>
               <View style={styles.mainContainer}>
-                <Text>{loading ? (<ActivityIndicator />) : error ? (<Text>Error found</Text>) : weatherData ? <Text>{ (parseFloat(weatherData.main.temp)-273).toFixed(2)}</Text>:null}</Text>
-                <Text>{loading ? (<ActivityIndicator />) : error ? (<Text>Error found</Text>) : weatherData ? <Text>{ weatherData.weather[0].main}</Text>:null}</Text>
+                <Text style={{color:'white',fontSize:60,fontWeight:'bold'}}>{loading ? (<ActivityIndicator />) : error ? (<Text></Text>) : weatherData ? <Text>{ (parseFloat(weatherData.main.temp)-273).toFixed(2)}°C</Text>:null}</Text>
+                <Text style={{color:'white',fontSize:24,fontWeight:'bold'}}>{loading ? (<ActivityIndicator />) : error ? (<Text></Text>) : weatherData ? <Text>{ weatherData.weather[0].main}</Text>:null}</Text>
               </View>
             </View>
-            <View>
-              <Text>{loading ? (<ActivityIndicator />) : error ? (<Text>Error found</Text>) : weatherData ? <Text>{ weatherData.main.pressure}</Text>:null}</Text>
-              <Text>{loading ? (<ActivityIndicator />) : error ? (<Text>Error found</Text>) : weatherData ? <Text>{ weatherData.main.humidity}</Text>:null}</Text>
-              <Text>{loading ? (<ActivityIndicator />) : error ? (<Text>Error found</Text>) : weatherData ? <Text>{ (parseFloat(weatherData.main.temp)-273).toFixed(2)}</Text>:null}</Text>
+            <View style={styles.lowerContainer}>
+              <View style={{flexDirection:'column', alignItems:'center'}}>
+                <Text style={{color:'white'}}>Pressure</Text>
+                <Text style={{color:'white'}}>{loading ? (<ActivityIndicator />) : error ? (<Text></Text>) : weatherData ? <Text>{ weatherData.main.pressure}</Text>:null}</Text>
+              </View>
+              <View style={{flexDirection:'column',alignItems:'center'}}>
+                <Text style={{color:'white'}}>Humidity</Text>
+                <Text style={{color:'white'}}>{loading ? (<ActivityIndicator />) : error ? (<Text></Text>) : weatherData ? <Text>{ weatherData.main.humidity}</Text>:null}</Text>
+              </View>
+              <View style={{flexDirection:'column',alignItems:'center'}}>
+                <Text style={{color:'white'}}>Feel Now</Text>
+                <Text style={{color:'white'}}>{loading ? (<ActivityIndicator />) : error ? (<Text></Text>) : weatherData ? <Text>{ (parseFloat(weatherData.main.feels_like)-273).toFixed(2)}</Text>:null}</Text>
+              </View>
             </View>
           </View>
         </View>
@@ -92,5 +110,14 @@ const styles = StyleSheet.create({
   mainContainer: {
     justifyContent: 'center',
     gap:10
+  },
+  lowerContainer: {
+    flexDirection: 'row',
+    borderTopWidth: 2,
+    paddingTop: 20,
+    justifyContent: 'space-evenly',
+    gap:70,
+    borderColor:'white'
+    
   }
 })
--- component/WeatherSearch.js
+++ component/WeatherSearch.js
@@ -4,7 +4,7 @@ import React from 'react'
 export default function weatherData({onPress,onChangeText}) {
   return (
             <View style={styles.searchInput}>
-                <TextInput style={{borderWidth:1,paddingVertical:1,fontSize:20}} placeholder='Enter the City' onChangeText={onChangeText} />
+                <TextInput style={{borderWidth:1,color:'white',paddingHorizontal:10,backgroundColor:'#382c3c', paddingVertical:1,borderRadius:7, fontSize:20,borderColor:'white',width:'70%',elevation:100}} placeholder='Type London' onChangeText={onChangeText} />
                 <TouchableOpacity onPress={onPress}>
                   <Text style={styles.btn}>Search</Text>
                 </TouchableOpacity>
@@ -18,5 +18,14 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         justifyContent: 'space-evenly',
         marginTop: 60,
-    }
+  },
+  btn: {
+    backgroundColor: '#211823',
+    paddingVertical: 5,
+    paddingHorizontal: 10,
+    borderRadius: 7,
+    elevation: 100,
+    color: 'white',
+    fontWeight:'bold'
+  }
 })
